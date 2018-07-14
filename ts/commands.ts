d2d7155908981a7592ff986bb28871a4b81515e2
tbhaxor <tbhaxor@gmail.com>
2018-07-14 20:40:56 +0530

    commands files added

---
+++ ts/commands.ts
@@ -0,0 +1,22 @@
+/*
+    command format
+    works: repl input
+
+    
+*/
+export default {
+    "globals": {
+        "help": "?",
+        "exit": "exit",
+        "clear": "clear",
+        "info": "info",
+        "lic": "lic"
+    },
+    "repl": {
+        "set": "set",
+        "describe": "desc",
+        "delete": "delete",
+        "show": "show",
+        "copy": "copy",
+    }
+}
